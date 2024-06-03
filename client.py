6318a5f4e8eefe652478ba0e675578d5892c4407
Mike Miller <mikemi@synamedia.com>
2024-06-03 05:58:00 +0300

    Pre-commit, tweaks

--- client.py
+++ client.py
@@ -1,39 +1,47 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,line-too-long,too-many-nested-blocks
 import socket
 import time
+import os
+import logging
 
 # Define the server address and port to connect to
-HOST = '127.0.0.1'  # Change to the server's IP address if it's remote
-PORT = 12345
+HOST = os.getenv("HOST", "127.0.0.1")
+PORT = int(os.getenv("PORT", "12345"))
+
+logging.basicConfig(level=logging.INFO)
+_LOGGER = logging.getLogger(__name__)
+
 
 def main():
     # Create a TCP/IP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     try:
         # Connect to the server
         client_socket.connect((HOST, PORT))
-        print(f'Connected to {HOST}:{PORT}')
-        
+        _LOGGER.info("Connected to %s:%s", HOST, PORT)
+        counter = 0
+
         while True:
             try:
-                # Send one byte of data
-                client_socket.sendall(b'\x01')
-                print('Sent one byte to the server')
-                
+                client_socket.sendall(str(counter).encode())
+                _LOGGER.info("Sent %d to the server", counter)
+                counter += 1
+
                 # Wait for one second before sending the next byte
                 time.sleep(1)
-            
+
             except (BrokenPipeError, ConnectionResetError) as e:
-                print(f'Connection error: {e}')
+                _LOGGER.warning("Connection error: %s", e)
                 break
-    
+
     except ConnectionRefusedError:
-        print(f'Failed to connect to {HOST}:{PORT}')
-    
+        _LOGGER.info("Failed to connect to %s:%s", HOST, PORT)
+
     finally:
         client_socket.close()
-        print('Connection closed')
+        _LOGGER.warning("Connection closed")
+
 
 if __name__ == "__main__":
     main()
-
--- server.py
+++ server.py
@@ -1,66 +1,78 @@
-import socket
+# pylint: disable=missing-module-docstring,missing-function-docstring,line-too-long,too-many-nested-blocks
+import logging
 import select
-import sys
+import socket
 
 # Define the server address and port
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 PORT = 12345
 
-def log_message(message):
-    print(message)
+logging.basicConfig(level=logging.INFO)
+_LOGGER = logging.getLogger(__name__)
+
 
 def main():
     # Create a TCP/IP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
+
     # Bind the socket to the server address and port
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
-    log_message(f'Server listening on {HOST}:{PORT}')
-    
+    _LOGGER.info("Server listening on %s:%s", HOST, PORT)
+
     # List of sockets to monitor for incoming connections
     sockets_list = [server_socket]
-    
+
     try:
         while True:
             # Use select to get the list of sockets ready for reading
-            read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-            
+            read_sockets, _, exception_sockets = select.select(
+                sockets_list, [], sockets_list
+            )
+
             for notified_socket in read_sockets:
                 # If the notified socket is the server socket, it means a new connection is coming in
                 if notified_socket == server_socket:
                     client_socket, client_address = server_socket.accept()
                     sockets_list.append(client_socket)
-                    log_message(f'Accepted new connection from {client_address}')
-                
+                    _LOGGER.info("Accepted new connection from %s", client_address)
+
                 # Otherwise, it's an existing connection sending data
                 else:
                     try:
                         message = notified_socket.recv(4096)
                         if message:
-                            log_message(f'Received message from {notified_socket.getpeername()}: {message.decode("utf-8")}')
+                            _LOGGER.info(
+                                "Received message from %s: %s",
+                                notified_socket.getpeername(),
+                                message.decode("utf-8"),
+                            )
                         else:
                             # No message means the client has closed the connection
-                            log_message(f'Connection closed by {notified_socket.getpeername()}')
+                            _LOGGER.warning(
+                                "Connection closed by %s", notified_socket.getpeername()
+                            )
                             sockets_list.remove(notified_socket)
                             notified_socket.close()
                     except ConnectionResetError:
-                        log_message(f'Connection reset by peer {notified_socket.getpeername()}')
+                        _LOGGER.warning(
+                            "Connection reset by peer %s", notified_socket.getpeername()
+                        )
                         sockets_list.remove(notified_socket)
                         notified_socket.close()
-            
+
             for notified_socket in exception_sockets:
-                log_message(f'Socket exception on {notified_socket.getpeername()}')
+                _LOGGER.warning("Socket exception on %s", notified_socket.getpeername())
                 sockets_list.remove(notified_socket)
                 notified_socket.close()
-    
+
     except KeyboardInterrupt:
-        log_message('Server is shutting down...')
-    
+        _LOGGER.info("Server is shutting down...")
+
     finally:
         server_socket.close()
 
+
 if __name__ == "__main__":
     main()
-
