5eeb649711c921cf771eb0e37ab15de101a3da1b
Mike Miller <github@mikeage.net>
2024-09-26 13:34:36 +0300

    Indent and add a hashbang to rabbitmq test script

--- rabbitmq/client/test.sh
+++ rabbitmq/client/test.sh
@@ -1,17 +1,16 @@
+#!/bin/bash
 kubectl apply -f rmqtest.yaml
 #kubectl apply -f rmqtest_external.yaml
 n=1
-while [ $n -lt 80 ]
-do
-sleep 5
-n=`expr $n + 3`
-kubectl get deployment -n default rmqtest
-kubectl get pods -n default|grep ago
-kubectl scale deployment -n default rmqtest --replicas=$n
+while [ $n -lt 80 ]; do
+	sleep 5
+	n=$(expr $n + 3)
+	kubectl get deployment -n default rmqtest
+	kubectl get pods -n default | grep ago
+	kubectl scale deployment -n default rmqtest --replicas=$n
 done
-while true
-do
-kubectl get deployment -n default rmqtest
-kubectl get pods -n default|grep ago
-sleep 5
+while true; do
+	kubectl get deployment -n default rmqtest
+	kubectl get pods -n default | grep ago
+	sleep 5
 done
